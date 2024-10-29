8e0961973e27f7cc3346c93683b5511da29b2034
dudux7 <eduardogerebatista556@gamial.com>
2024-10-29 16:53:02 -0300

    upando style registro

---
+++ frontend/src/views/SignUpView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <SignUpVue />
+  </div>
+</template>
+
+  <script>
+  import SignUpVue from '../components/SignUp.vue'
+
+    export default {
+      components: {
+        SignUpVue
+      }
+    }
+  </script>
+
+<style>
+
+</style>
